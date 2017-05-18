b86f343eb6a781bf9faf18cfc501df0bb6701e7d
Eugene Mironov <helper2424@gmail.com>
2017-05-18 12:26:27 +0500

    Fixed last changes

--- lib/exlibris-alma.rb
+++ lib/exlibris-alma.rb
@@ -2,15 +2,15 @@ require 'logger'
 require 'rest-client'
 require 'webmock'
 require 'vcr'
+require 'marcus'
 
 require 'exlibris/base'
-require 'exlibris/alma/api'
 
 module Exlibris
   module Alma
-    BASE_URL_AMERICA = 'https://api-na.hosted.exlibrisgroup.com'
-    BASE_URL_EUROPE = 'https://api-eu.hosted.exlibrisgroup.com'
-    BASE_URL_ASIA_PACIFIC = 'https://api-ap.hosted.exlibrisgroup.com'
-    BASE_URL_CANADA ='https://api-ca.hosted.exlibrisgroup.com'
+    BASE_URL_AMERICA = 'https://api-na.hosted.exlibrisgroup.com'.freeze
+    BASE_URL_EUROPE = 'https://api-eu.hosted.exlibrisgroup.com'.freeze
+    BASE_URL_ASIA_PACIFIC = 'https://api-ap.hosted.exlibrisgroup.com'.freeze
+    BASE_URL_CANADA ='https://api-ca.hosted.exlibrisgroup.com'.freeze
   end
 end
---
+++ spec/spec_helper.rb
@@ -0,0 +1,16 @@
+$LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
+require 'exlibris-alma.rb'
+require 'rspec/its'
+require 'vcr'
+require 'webmock/rspec'
+
+VCR.configure do |c|
+  c.ignore_localhost = true
+  c.cassette_library_dir = 'spec/fixtures/vcr_cassettes'
+  c.hook_into :webmock
+  c.configure_rspec_metadata!
+  def_co = c.default_cassette_options
+  def_co[:record] = :new_episodes
+  c.default_cassette_options = def_co
+  c.allow_http_connections_when_no_cassette = true
+end
