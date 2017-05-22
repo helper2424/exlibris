29b686f8e159d4af46a167483c78a6c83cbd249f
Eugene Mironov <helper2424@gmail.com>
2017-05-22 22:19:33 +0500

    Added holdings, loans and items api methods

--- lib/exlibris/alma/api.rb
+++ lib/exlibris/alma/api.rb
@@ -10,14 +10,34 @@ module Exlibris
         @version = 1
       end
 
+      # See https://developers.exlibrisgroup.com/alma/apis/bibs/GET/gwPcGly021q2Z+qBbnVJzw==/af2fb69d-64f4-42bc-bb05-d8a0ae56936e
       def check_availability(mms_id)
         data = { expand: 'p_avail,e_avail,d_avail' }
         response = request method: :get, url_body: "bibs/#{mms_id}",
                            data: data
         return nil if response.nil?
-        xml = Nokogiri::XML response.body
-        xml.remove_namespaces!
-        Hash[xml.xpath('//bib/*').map { |row| [row.name, row.text] }]
+        Oj.load response.body
+      end
+
+      # See https://developers.exlibrisgroup.com/alma/apis/bibs/GET/gwPcGly021om4RTvtjbPleCklCGxeYAfEqJOcQOaLEvEGUPgvJFpUQ==/af2fb69d-64f4-42bc-bb05-d8a0ae56936e
+      def holdings(mms_id)
+        response = request method: :get, url_body: "bibs/#{mms_id}/holdings"
+        return nil if response.nil?
+        Oj.load response.body
+      end
+
+      # See https://developers.exlibrisgroup.com/alma/apis/bibs/GET/gwPcGly021om4RTvtjbPleCklCGxeYAfY09xr4k8GXw=/af2fb69d-64f4-42bc-bb05-d8a0ae56936e
+      def loans(mms_id)
+        response = request method: :get, url_body: "bibs/#{mms_id}/loans"
+        return nil if response.nil?
+        Oj.load response.body
+      end
+
+      # See https://developers.exlibrisgroup.com/alma/apis/bibs/GET/gwPcGly021om4RTvtjbPleCklCGxeYAfEqJOcQOaLEvNcHQT0/ozqu3DGTurs/XxIP4LrexQUdc=/af2fb69d-64f4-42bc-bb05-d8a0ae56936e
+      def holding_items(mms_id, holding_id)
+        response = request method: :get, url_body: "bibs/#{mms_id}/holdings/#{holding_id}/items"
+        return nil if response.nil?
+        Oj.load response.body
       end
 
       private
@@ -28,6 +48,7 @@ module Exlibris
 
       def request(method:, url_body:, data: {}, payload: nil)
         data[:apikey] = api_key
+        data[:format] = :json
 
         begin
           response = RestClient::Request.execute method: method, url: build_url(url_body),
